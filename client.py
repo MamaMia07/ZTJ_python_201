243e211ffa5f4e5ba97c691419fea10c119eb3c7
MamaMia07 <kokoszkam@gazeta.pl>
2023-07-09 02:00:48 +0200

    class Response

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 HOST = "127.0.0.1"  # "156.17.181.78" # "127.0.0.1"  # The server's hostname or IP address
 PORT = 9090  # The port used by the server
@@ -11,10 +12,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clnt_socket:
         request = input("> ")
         #clnt_socket.send("Hello, world!! It's ME!!!".encode("utf-8"))
         clnt_socket.send(request.encode("utf-8"))
-        data = clnt_socket.recv(1024)
-        
+        data = ""
+        while True:
+            rec = clnt_socket.recv(1024)
+            if len(rec)<=0:
+                break
+            data += rec.decode("utf-8")
+        #data = clnt_socket.recv(1024)
         if request =="stop": connect = False
-        print("Received data: ")
-        print(data.decode('utf-8'))
+        #print("Received data: ")
+        print(data) #.decode('utf-8'))
             
 
--- server.py
+++ server.py
@@ -1,18 +1,33 @@
 # echo-server.py
 import socket
 import time
+import json
 
-class Message():
+class Response():
     def __init__(self, command):
         self.command = command
+        self.commands = ["stop", "help", "uptime","info"]
 
-    def help_msg(self):
+    def stop_resp(self, add):
+        print(f"Connection terminated by {add[0]}")
+        conn_socket.send(f"Unconnected".encode('utf-8'))
+        
+    def help_resp(self):
         pass
-    def
-        
-        
-    def send_message(self):
-        if self.command == "help": help_msg()
+
+    def uptime_resp(self):
+        pass
+
+    def version_resp(self):
+        pass
+
+    def unknown_cmd_resp(self):
+        pass
+
+    def send_response(self):
+        pass
+        #if self.command not in self.commands: unknown_cmd_resp(self) 
+        #if self.command == "help": help_msg()
         
 
 
@@ -21,29 +36,34 @@ HOST = "127.0.0.1"  #"156.17.181.78" # "127.0.0.1"  # Standard loopback interfac
 PORT = 9090  # Port to listen on (non-privileged ports are > 1023)
 start_server = time.time()
 
-message = message()
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.bind((HOST, PORT))
     server.listen()
     conn_socket, address = server.accept()
+
     with conn_socket:
         print(f"Connected with {address[0]}")
         while True:
-            data_recv = conn_socket.recv(1024)
-            data = data_recv.decode('utf-8)
+            #data_recv = conn_socket.recv(1024)
+            data = conn_socket.recv(1024).decode("utf-8")
 
-            if data.decode('utf-8') == "stop":
-                print(f"Connection terminated by {address[0]}")
-                conn_socket.send(f"Unconnected".encode('utf-8'))
+            resp = Response(data)
+
+##            if data.decode("utf-8") == "stop":
+##                print(f"Connection terminated by {address[0]}")
+##                conn_socket.send(f"Unconnected".encode('utf-8'))
+            if data == "stop":
+                resp.stop_resp(address)
                 break
 
-            elif data.decode('utf-8') == "info":
+            elif data == "info": #data.decode('utf-8') == "info":
                  conn_socket.send(f"info: to jest wersja {version}".encode('utf-8'))
           
-            elif data.decode('utf-8') == "uptime":
+            elif data == "uptime": #data.decode('utf-8') == "uptime":
                  server_life = time.time() - start_server
                  conn_socket.send(f"czas życia servera: {server_life:0.4f}s".encode('utf-8'))
 
             else:   
-                conn_socket.send(f"Got your message! Thank you!\nit was: {data.decode('utf-8')}".encode('utf-8'))
-            print(data.decode('utf-8'))
+                conn_socket.send(f"Got your message! Thank you!\nit was: {data}".encode('utf-8'))
+            print(data) #(data.decode('utf-8'))
