d02cd93576ab7038396eaac942cb551e61ad4b14
MamaMia07 <kokoszkam@gazeta.pl>
2023-07-10 20:55:59 +0200

    while

--- client.py
+++ client.py
@@ -13,14 +13,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clnt_socket:
         #clnt_socket.send("Hello, world!! It's ME!!!".encode("utf-8"))
         clnt_socket.send(request.encode("utf-8"))
         data = ""
-        while True:
-            rec = clnt_socket.recv(1024)
-            if len(rec)<=0:
-                break
-            data += rec.decode("utf-8")
-        #data = clnt_socket.recv(1024)
+##        while True:
+##            rec = clnt_socket.recv(1024)
+##            if len(rec)<=0:
+##                break
+##            data += rec.decode("utf-8")
+        data = clnt_socket.recv(1024)
         if request =="stop": connect = False
         #print("Received data: ")
-        print(data) #.decode('utf-8'))
+        print(data.decode('utf-8'))
             
 
